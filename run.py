9db495fd3b58dba41647609e5397fb110d6f6ca0
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-03-01 14:25:15 +0000

    testing text colors for welcome message

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ import time
  
 # Variables for the player and computer game board
 
-PLAYER_BOARD = [[" "] * 8 for i in range(8)]
-COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
-PLAYER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
-COMPUTER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
+PLAYER_BOARD = [[" "] * 10 for i in range(10)]
+COMPUTER_BOARD = [[" "] * 10 for i in range(10)]
+PLAYER_GUESS_BOARD = [[" "] * 10 for i in range(10)]
+COMPUTER_GUESS_BOARD = [[" "] * 10 for i in range(10)]
 
 # Converting strings into integers
 LETTER_TRANSLATE = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
@@ -15,9 +15,13 @@ LETTER_TRANSLATE = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H":
 SEPERATOR = "~" * 80
 
 # The opening message that a user will see when PirateShips runs
-def welcome_message():
-    print("Ahoy Matey, welcome to PirateShips defeat your enemies make way with the treasures!")
-    print('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+print(SEPERATOR)
+
+def welcome_message(): 
+    print("\033[1;32;40m Ahoy Matey, welcome to PirateShips defeat your enemies")
+    print("\033[1;32;40m and make way with the treasures!")
+    print("\033[1;32;40m The high sea's have but a few rules:")
+
 
 
 
