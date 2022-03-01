0d1b7f498614b881f442519110e0d8dbad1a2094
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-03-01 15:13:53 +0000

    fixed text background color and spacing for welcome message

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ COMPUTER_GUESS_BOARD = [[" "] * 10 for i in range(10)]
 LETTER_TRANSLATE = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
 
 # Seperator creates a line break
-SEPERATOR = "\033[1;35;40m~" * 80
+SEPERATOR = "\033[1;35;48m~" * 80
 
 # The opening message that a user will see when PirateShips runs
 
@@ -23,17 +23,19 @@ def welcome_message():
     see when PirateShips runs
     """
     print(SEPERATOR)
-    print("\033[1;32;40m Ahoy Matey, welcome to PirateShips defeat your enemies")
-    print("\033[1;32;40m and make way with the treasures!\n")
-    print("""\033[1;31;40m The aim of the game is to destroy the enemy fleet,
- before they destroy yours. The winner takes away endless treasure
- and more importantly their name will become legend\n""")
+    print("\033[1;32;48mAhoy Matey, welcome to PirateShips defeat your enemies")
+    print("\033[1;32;48mand make way with the treasures!\n")
+    print("""\033[1;31;48mThe aim of the game is to destroy the enemy fleet,
+before they destroy yours. The winner takes away endless treasure
+and more importantly their name will become legend\n""")
 
-    print("\033[1;32;40m The high sea's have but a few rules:\n")
-    print("\033[1;31;40m Rule #1: Have no mercy on the enemy")
-    print("\033[1;31;40m Rule #2: All ships must be placed within the board")
-    print("\033[1;31;40m Rule #3: The first to hit the enemy ships 15 times wins\n")
+    print("\033[1;32;48mThe high sea's have but a few rules:\n")
+    print("\033[1;31;48mRule #1: Have no mercy on the enemy")
+    print("\033[1;31;48mRule #2: All ships must be placed within the board")
+    print("\033[1;31;48mRule #3: The first to hit the enemy ships 15 times wins\n")
     print(SEPERATOR)
+    
+
 
 
 
