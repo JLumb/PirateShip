31fdb3fde7b627e44b3b579bfa9f2d713248ab3f
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-03-01 13:37:45 +0000

    created variables for the game board and converting strings to int

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
+
+PLAYER_BOARD = [[" "]] * 10 for i in range(10)]
+COMPUTER_BOARD = [[" "]] * 10 for i in range(10)]
+PLAYER_GUESS = [[" "]] * 10 for i in range(10)]
+COMPUTER_GUESS = [[" "]] * 10 for i in range(10)]
+
+LETTER_TRANSLATE = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
