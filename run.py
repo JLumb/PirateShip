f76ba03bc55a465718f789a46f702fd75c19788f
Josh Lumb <JoshLumby@hotmail.co.uk>
2022-03-02 14:07:36 +0000

    create the print game board function

--- run.py
+++ run.py
@@ -59,10 +59,16 @@ def verify_username(username):
         return True
 
 
-
-
-
-
+def print_board(board):
+    """
+    this function prints the game board out throughtout the project
+    """
+    print"A B C D E F G H"
+    print"~ ~ ~ ~ ~ ~ ~ ~"
+    row_number = 1
+    for row in board:
+        print("%d|%s" % (row_number, "|".join(row)))
+        row_number += 1
 
 
 
